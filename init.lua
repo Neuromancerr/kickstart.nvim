b290d81e561d9b4b78d22ddcdf544c3c102430b0
Schmillet <andrewkolkmeier@gmail.com>
2024-04-09 17:48:11 -0500

    FINALLY fixed tabs

--- init.lua
+++ init.lua
@@ -154,7 +154,7 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
-vim.o.softtabstop = 4
+vim.o.tabstop = 4
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
