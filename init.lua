2d42159dc79415deb396463a921566cc0fc2a2d8
Schmillet <andrewkolkmeier@gmail.com>
2025-03-25 18:23:27 -0500

    Resolved depreciated tsserver error

--- init.lua
+++ init.lua
@@ -468,7 +468,7 @@ require('lazy').setup({
                 --    https://github.com/pmizio/typescript-tools.nvim
                 --
                 -- But for many setups, the LSP (`tsserver`) will work just fine
-                tsserver = {},
+                ts_ls = {},
                 angularls = {},
                 --
 
