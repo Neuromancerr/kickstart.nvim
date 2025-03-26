a05dd32d63b985d41a8348cb9219e5029d086df1
Neuromancerr <andrewkolkmeier@gmail.com>
2025-03-26 12:12:40 -0500

    Minor changes with theme

--- init.lua
+++ init.lua
@@ -202,7 +202,6 @@ require('lazy').setup({
         event = 'VimEnter', -- Sets the loading event to 'VimEnter'
         config = function() -- This is the function that runs, AFTER loading
             require('which-key').setup()
-
         end,
     },
 
@@ -672,14 +671,23 @@ require('lazy').setup({
         -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
         'rebelot/kanagawa.nvim',
         priority = 1000, -- Make sure to load this before all the other start plugins.
-        init = function()
-            -- Load the colorscheme here.
-            -- Like many other themes, this one has different styles, and you could load
-            -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-            vim.cmd.colorscheme 'kanagawa-dragon'
+        --init = function()
+        -- Load the colorscheme here.
+        -- Like many other themes, this one has different styles, and you could load
+        -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+        --vim.cmd.colorscheme 'kanagawa-dragon'
 
-            -- You can configure highlights by doing something like:
-            vim.cmd.hi 'Comment gui=none'
+        -- You can configure highlights by doing something like:
+        --vim.cmd.hi 'Comment gui=none'
+        --end,
+    },
+
+    {
+        'scottmckendry/cyberdream.nvim',
+        lazy = false,
+        priority = 1000,
+        init = function()
+            vim.cmd.colorscheme 'cyberdream'
         end,
     },
 
@@ -689,22 +697,6 @@ require('lazy').setup({
         event = 'VimEnter',
         dependencies = { 'nvim-lua/plenary.nvim' },
         opts = { signs = false },
-    },
-    -- Love2D Highlighting and Docs
-    {
-        'davisdude/vim-love-docs',
-    },
-
-    -- Love2D Support
-    {
-        'S1M0N38/love2d.nvim',
-        cmd = 'LoveRun',
-        opts = {},
-        keys = {
-            { '<leader>v', desc = 'LOVE' },
-            { '<leader>vv', '<cmd>LoveRun<cr>', desc = 'Run LOVE' },
-            { '<leader>vs', '<cmd>LoveStop<cr>', desc = 'Stop LOVE' },
-        },
     },
 
     { -- Collection of various small independent plugins/modules
@@ -724,21 +716,6 @@ require('lazy').setup({
             -- - sd'   - [S]urround [D]elete [']quotes
             -- - sr)'  - [S]urround [R]eplace [)] [']
             require('mini.surround').setup()
-
-            -- Simple and easy statusline.
-            --  You could remove this setup call if you don't like it,
-            --  and try some other statusline plugin
-            local statusline = require 'mini.statusline'
-            -- set use_icons to true if you have a Nerd Font
-            statusline.setup { use_icons = vim.g.have_nerd_font }
-
-            -- You can configure sections in the statusline by overriding their
-            -- default behavior. For example, here we set the section for
-            -- cursor location to LINE:COLUMN
-            ---@diagnostic disable-next-line: duplicate-set-field
-            statusline.section_location = function()
-                return '%2l:%-2v'
-            end
 
             -- ... and there is more!
             --  Check out: https://github.com/echasnovski/mini.nvim
