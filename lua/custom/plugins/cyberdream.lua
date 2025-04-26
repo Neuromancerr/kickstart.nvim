a9df990695f96c35838c0773266a1d1749a900a2
Neuromancerr <andrewkolkmeier@gmail.com>
2025-04-26 14:16:35 -0500

    Refactored NeoVim plugins to custom folder for ease of access and customization

---
+++ lua/custom/plugins/cyberdream.lua
@@ -0,0 +1,14 @@
+return --NVIM Theme, Set to match transparency of terminal
+{
+	'scottmckendry/cyberdream.nvim',
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require('cyberdream').setup {
+			transparent = true,
+		}
+	end,
+	init = function()
+		vim.cmd.colorscheme 'cyberdream'
+	end,
+}
