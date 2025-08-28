672bf6f85659c1650ed2c065b65826274c2f2e4a
Neuromancerr <andrewkolkmeier@gmail.com>
2025-08-28 14:59:21 -0500

    Removed cyberdream, added clang fomatting to comform

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -31,6 +31,8 @@ return { -- Autoformat
 			-- You can use a sub-list to tell conform to run *until* a formatter
 			-- is found.
 			javascript = { 'prettier' },
+			c = { 'clang_format' },
+			cpp = { 'clang_format' },
 		},
 	},
 }
